df72e06771847a3e4c6430e7e4615a6ae7ae3689
Hoang Tran <hoang.tran@uclouvain.be>
2018-08-01 18:35:22 +0200

    set up netns + bridge to use tc
    my_net.sh manually setups networking without depending on mininet:
    - Set up netns for server and client
    - Set up bridge connecting the two netns
    - Add interfaces in netns and bridge
    - Add tc tbf qdisc to control bw, loss and delay
    - Calls the server and client app

---
+++ mptcp-bpf/my_net.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# set up netns + bridge to use traffic control
+#
+# server is in ns1
+# |_veth1________|
+#    |
+#  __|___________
+# | ethBr1       |
+# |   \          |
+# |   Bridge    nsBr
+# |   /          |
+# | ethBr2       |
+# |__|___________|
+#  __|___________
+# | veth2        |
+# client is in ns2
+
+NS1="ip netns exec ns1 "
+NS2="ip netns exec ns2 "
+NS_BR="ip netns exec nsBr "
+
+set -x
+
+sysctl -w net.mptcp.mptcp_debug=1
+sysctl -w net.mptcp.mptcp_path_manager=ndiffports
+
+# Clean
+$NS_BR ip link del ethBr1
+$NS_BR ip link del ethBr2
+$NS_BR ip link del dev br
+$NS1 ip link del veth1
+$NS2 ip link del veth2
+
+ip netns del ns1
+ip netns del ns2
+ip netns del nsBr
+
+# Add namespaces
+ip netns add ns1
+ip netns add ns2
+ip netns add nsBr
+
+# Add veths interfaces
+ip link add veth1 type veth peer name ethBr1
+ip link add veth2 type veth peer name ethBr2
+
+#link veths
+ip link set netns ns1 veth1
+ip link set netns ns2 veth2
+ip link set netns nsBr ethBr1
+ip link set netns nsBr ethBr2
+
+#assign mac's
+$NS1  ifconfig veth1 hw ether 02:03:01:04:06:07
+$NS2  ifconfig veth2 hw ether 02:03:01:04:05:06
+$NS_BR ifconfig ethBr1 hw ether 02:03:06:05:07:04
+$NS_BR ifconfig ethBr2 hw ether 02:03:06:05:07:05
+
+#assign ip's
+$NS1 ifconfig veth1 10.1.1.1/24 up
+$NS2 ifconfig veth2 10.1.1.2/24 up
+
+#setup bridge
+brctl addbr br
+ip link del dev br
+$NS_BR brctl addbr br
+$NS_BR brctl addif br ethBr1
+$NS_BR ip link set up dev ethBr1
+$NS_BR brctl addif br ethBr2
+$NS_BR ip link set up dev ethBr2
+
+$NS_BR ip link set up dev br
+
+#add delay and bw
+# for client-to-server traffic
+$NS_BR tc qdisc add dev ethBr1   root handle 1:0    netem delay 40ms loss 0.5%
+$NS_BR tc qdisc add dev ethBr1   parent 1:1 handle 10:    tbf rate 40Mbit latency 2ms burst 10000
+
+# for server-to-client traffic
+$NS_BR tc qdisc add dev ethBr2   root handle 1:0    netem delay 40ms loss 0.5%
+$NS_BR tc qdisc add dev ethBr2   parent 1:1 handle 10:    tbf rate 40Mbit latency 2ms burst 10000
+
+
+serverIP="10.1.1.1"
+serverPort=80
+time=`date +%s`
+dump_server=$time+"-server.pcap"
+dump_client=$time+"-client.pcap"
+
+#$NS1  tcpdump -i veth1 -w dump_server &
+#$NS2  tcpdump -i veth2 -w dump_client &
+$NS_BR  tcpdump -i ethBr1 -w dump_server_br &
+$NS_BR  tcpdump -i ethBr2 -w dump_client_br &
+$NS_BR  tcpdump -i br -w dump_br &
+
+
+$NS1  python3 -m http.server 80 &
+
+sleep 1
+
+# client will self-terminate in (-m) seconds
+$NS2  curl $serverIP:$serverPort/vmlinux.o  -m 3  -o /dev/null
+
+#pkill tcpdump
+pkill tcpdump
+pkill tcpdump
+pkill tcpdump
+
+pkill python3
