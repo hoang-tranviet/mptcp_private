ae3305de8a54b7dccb7fa699e716ae278fb9bebf
Hoang Tran <hoang.tran@uclouvain.be>
2018-08-16 19:36:27 +0200

    add bpf hook: to set data-ack-bytes threshold
    reinject data-ack if it acknowledges at least an amount of bytes

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -1298,6 +1298,8 @@ static int mptcp_alloc_mpcb(struct sock *meta_sk, __u64 remote_key,
 	mpcb->rtt_threshold = 0x7FFFFFFF;	/* set initial threshold as maximum signed integer */
 	mpcb->backup_sfs_mode = 0;
 
+	mpcb->acked_bytes_threshold = 0x7FFFFFFF;
+
 	/* The meta is directly linked - set refcnt to 1 */
 	refcount_set(&mpcb->mpcb_refcnt, 1);
 
