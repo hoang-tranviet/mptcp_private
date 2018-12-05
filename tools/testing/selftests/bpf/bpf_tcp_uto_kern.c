10f24b6497410cd8d7205f6edb9ec3ac4fe49469
Hoang Tran <hoang.tran@uclouvain.be>
2018-12-05 11:17:58 +0100

    minimized bpf programs for LoC count

--- tools/testing/selftests/bpf/bpf_tcp_uto_kern.c
+++ tools/testing/selftests/bpf/bpf_tcp_uto_kern.c
@@ -8,41 +8,9 @@
 #include <linux/types.h>
 #include <linux/socket.h>
 #include <linux/tcp.h>
-//#include <netinet/in.h>
 #include "bpf_helpers.h"
 #include "bpf_endian.h"
 #include "test_tcpbpf.h"
-
-struct bpf_map_def SEC("maps") global_map = {
-	.type = BPF_MAP_TYPE_ARRAY,
-	.key_size = sizeof(__u32),
-	.value_size = sizeof(struct tcpbpf_globals),
-	.max_entries = 2,
-};
-
-static inline void update_event_map(int event)
-{
-	__u32 key = 0;
-	struct tcpbpf_globals g, *gp;
-
-	gp = bpf_map_lookup_elem(&global_map, &key);
-	if (gp == NULL) {
-		struct tcpbpf_globals g = {0};
-
-		g.event_map |= (1 << event);
-		bpf_map_update_elem(&global_map, &key, &g,
-			    BPF_ANY);
-	} else {
-		g = *gp;
-		g.event_map |= (1 << event);
-		bpf_map_update_elem(&global_map, &key, &g,
-			    BPF_ANY);
-	}
-}
-
-static inline unsigned int swap(unsigned int num) {
-	return __builtin_bswap32(num);
-}
 
 int _version SEC("version") = 1;
 
@@ -62,90 +30,42 @@ int bpf_testcb(struct bpf_sock_ops *skops)
 		.len = 4,   // of this option struct
 		.data = 0x0100, // 1 second
 	};
-
 	int rv = -1;
 	int granularity = 0;
-	int op;
-	int v = 0;
 	int option_buffer;
 	unsigned int uto, UserTimeout;
 
-	op = (int) skops->op;
-
-	update_event_map(op);
+	int op = (int) skops->op;
 
 	switch (op) {
 	case BPF_SOCK_OPS_TCP_CONNECT_CB:
-		/* Set specific callback */
 		rv = bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_OPTION_WRITE_FLAG);
-
 		UserTimeout = 1000;
 		bpf_setsockopt(skops, IPPROTO_TCP, TCP_BPF_USER_TIMEOUT, &UserTimeout, sizeof(UserTimeout));
-
-		bpf_getsockopt(skops, IPPROTO_TCP, TCP_BPF_USER_TIMEOUT, &UserTimeout, sizeof(UserTimeout));
-		char fmt111[] = "client Timeout: %d\n";
-		bpf_trace_printk(fmt111, sizeof(fmt111), UserTimeout);
-
-		char fmt0[] = "tcp connect callback\n";
-		bpf_trace_printk(fmt0, sizeof(fmt0));
 		break;
-	case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
-	{
-		char fmt2[] = "active established callback\n";
-		bpf_trace_printk(fmt2, sizeof(fmt2));
-		break;
-	}
 	case BPF_TCP_OPTIONS_SIZE_CALC:
 	{
 		int option_len = sizeof(struct tcp_option);
-		/* args[1] is the second argument */
-		if (skops->args[1] + option_len <= 40) {
+		if (skops->args[1] + option_len <= 40)
 			rv = option_len;
-			char fmt4[] = "OPTIONS_SIZE_CALC  \t original:%d extend:%d B\n";
-			//bpf_trace_printk(fmt4, sizeof(fmt4), skops->args[1], option_len);
-		}
 		else rv = 0;
 		break;
 	}
 	case BPF_TCP_OPTIONS_WRITE:
-		/* put the struct option into the reply value */
 		if (granularity != 0) {
-			/* set granularity flag */
 			struct tcp_option *p = (&opt);
 			p->data |= G_FLAG;
 		}
-
 		memcpy(&option_buffer, &opt, sizeof(int));
-		// skops->reply_long = opt;
 		rv = option_buffer;
-
-		char fmt5[] = "OPTIONS_WRITE: %x \n\n";
-		bpf_trace_printk(fmt5, sizeof(fmt5), rv);
 		break;
-
 	case BPF_TCP_PARSE_OPTIONS:
-		/* get original value */
-		rv = bpf_getsockopt(skops, IPPROTO_TCP, TCP_BPF_USER_TIMEOUT, &UserTimeout, sizeof(UserTimeout));
-		char fmt11[] = "real Timeout: %d\n";
-		bpf_trace_printk(fmt11, sizeof(fmt11), UserTimeout);
-
-		/* get the parsed option, swap to little-endian */
-		uto = swap(skops->args[2]);
-
-		/* Keep the last 15 bits */
+		uto = __builtin_bswap32(skops->args[2]);
 		UserTimeout = (uto & 0x00007FFF)*1000;
 		granularity = (uto & 0x00008000) >> 15;
-
-		char fmt10[] = "PARSE_OPTIONS: data: %x, granu: %x, UTO: %d\n";
-		bpf_trace_printk(fmt10, sizeof(fmt10), uto, granularity, UserTimeout);
 		if (granularity != 0)
-			/* convert from minutes to seconds */
 			UserTimeout *= 60;
 		rv = bpf_setsockopt(skops, IPPROTO_TCP, TCP_BPF_USER_TIMEOUT, &UserTimeout, sizeof(UserTimeout));
-
-		/* get new UTO */
-		rv = bpf_getsockopt(skops, IPPROTO_TCP, TCP_BPF_USER_TIMEOUT, &UserTimeout, sizeof(UserTimeout));
-		bpf_trace_printk(fmt11, sizeof(fmt11), UserTimeout);
 		break;
 	default:
 		rv = -1;
