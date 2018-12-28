5953535d114d3c840d65a7f4c8b243bba7d8e62a
Hoang Tran <hoang.tran@uclouvain.be>
2018-12-28 19:04:40 +0100

    bpf: add hook BPF_MPTCP_NEW_SESSION

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -1485,6 +1485,10 @@ void mptcp_del_sock(struct sock *sk)
  */
 void mptcp_update_metasocket(const struct sock *meta_sk)
 {
+	struct tcp_sock *mtp = tcp_sk(meta_sk);
+
+	tcp_call_bpf(meta_sk, BPF_MPTCP_NEW_SESSION);
+
 	if (tcp_sk(meta_sk)->mpcb->pm_ops->new_session)
 		tcp_sk(meta_sk)->mpcb->pm_ops->new_session(meta_sk);
 }
