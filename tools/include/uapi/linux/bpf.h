8e954d8c6f03121300c36014cb197a5946a79726
Hoang Tran <hoang.tran@uclouvain.be>
2018-12-30 20:15:30 +0100

    bpf: add hook BPF_MPTCP_FULLY_ESTABLISHED

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -1178,6 +1178,7 @@ enum {
 	BPF_MPTCP_ADD_SOCK,		/* Called when a new sock is added to
 					 * an MPTCP connection */
 	BPF_MPTCP_NEW_SESSION,		/* Called when a new MPTCP session is created */
+	BPF_MPTCP_FULLY_ESTABLISHED,	/* Called when an MPTCP session is fully established */
 };
 
 /* List of TCP states. There is a build check in net/ipv4/tcp.c to detect
