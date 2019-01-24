feb9d58e124ca7277cee425009bc4bdb029cf456
Hoang Tran <hoang.tran@uclouvain.be>
2019-01-24 08:03:28 -0800

    pm prog: add fist bpf-pm program

---
+++ mptcp-bpf/bpf_mptcp_pm.c
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <stddef.h>
+#include <string.h>
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#include <linux/if_packet.h>
+#include <linux/ip.h>
+#include <linux/types.h>
+#include <linux/socket.h>
+#include <linux/tcp.h>
+#include "bpf_helpers.h"
+#include "bpf_endian.h"
+#include "test_tcpbpf.h"
+
+int _version SEC("version") = 1;
+
+SEC("sockops")
+int bpf_testcb(struct bpf_sock_ops *skops)
+{
+	int rv = -1;
+	int op;
+	int v = 0;
+
+	op = (int) skops->op;
+
+	switch (op) {
+	case BPF_MPTCP_NEW_SESSION:
+	{
+		char snew[] = "%x: new mptcp connection\n";
+		bpf_trace_printk(snew, sizeof(snew), skops->mptcp_loc_token);
+		break;
+	}
+	case BPF_MPTCP_ADD_SOCK:
+	{
+		unsigned int id = skops->args[0];
+		char fmt1[] = "%x: SYN-ACK arrived: subflow id: %u \n";
+		bpf_trace_printk(fmt1, sizeof(fmt1), skops->mptcp_loc_token, id);
+		break;
+	}
+	default:
+		rv = -1;
+	}
+	skops->reply = rv;
+	return 1;
+}
+char _license[] SEC("license") = "GPL";
