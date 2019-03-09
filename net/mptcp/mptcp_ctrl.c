dec8577d7ba9ce03a77e0479a765a1b7d2fab264
Hoang Tran <hoang.tran@uclouvain.be>
2019-03-08 16:25:20 -0800

    remove unused mtp in MPTCP_OPEN_SESSION

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -1557,8 +1557,6 @@ void mptcp_del_sock(struct sock *sk)
  */
 void mptcp_update_metasocket(const struct sock *meta_sk)
 {
-	struct tcp_sock *mtp = tcp_sk(meta_sk);
-
 	tcp_call_bpf(meta_sk, BPF_MPTCP_NEW_SESSION, 0, NULL);
 
 	if (tcp_sk(meta_sk)->mpcb->pm_ops->new_session)
