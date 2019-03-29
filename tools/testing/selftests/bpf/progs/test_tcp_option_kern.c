a95ded0f9f4b2b3fea59f490c0eb77c7f3e5666f
Hoang Tran <hoang.tran@uclouvain.be>
2019-03-29 10:42:04 +0100

    selftests/bpf: add a test for tcp_option hooks
    This selftest is similar to test_tcpbpf.
    
    Here, the client inserts new TCP option to each outgoing segment and
    the selftest checks if the option parsing hook is triggered on the
    server side.

---
+++ tools/testing/selftests/bpf/progs/test_tcp_option_kern.c
@@ -0,0 +1,80 @@
+/* Copyright (c) 2019 Viet-Hoang Tran */
+#include <string.h>
+#include <linux/bpf.h>
+#include "bpf_helpers.h"
+#include "bpf_endian.h"
+
+int _version SEC("version") = 1;
+
+struct bpf_map_def SEC("maps") option_count = {
+	.type = BPF_MAP_TYPE_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(int),
+	.max_entries = 2,
+};
+
+struct tcp_option {
+	__u8 kind;
+	__u8 len;
+	__u16 data;
+};
+
+#define IW 5
+
+SEC("sockops")
+int test_tcp_option(struct bpf_sock_ops *skops)
+{
+	struct tcp_option opt = {
+		.kind = 66, 	// arbitrary
+		.len  = 4,   	// of this option struct
+		.data = bpf_htons(IW),
+	};
+
+	__u32 option_buffer, tcp_options_size;
+	__u32 rcv_opt, opt_val, key;
+	int rv = 0;
+
+	switch (skops->op) {
+	/* client side */
+	case BPF_SOCK_OPS_TCP_CONNECT_CB:
+		rv = bpf_sock_ops_cb_flags_set(skops,
+					BPF_SOCK_OPS_OPTION_WRITE_FLAG);
+		break;
+	case BPF_TCP_OPTIONS_SIZE_CALC:
+		tcp_options_size = skops->args[1];
+		if (tcp_options_size + sizeof(opt) <= 40)
+			rv = sizeof(opt);
+		else
+			rv = 0;
+		break;
+	case BPF_TCP_OPTIONS_WRITE:
+		/* put the struct option into the reply value */
+		memcpy(&option_buffer, &opt, sizeof(int));
+		rv = option_buffer;
+		break;
+
+	/* server side */
+	case BPF_SOCK_OPS_TCP_LISTEN_CB:
+		rv = bpf_sock_ops_cb_flags_set(skops,
+					BPF_SOCK_OPS_OPTION_PARSE_FLAG);
+		break;
+	case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+		key = 0;
+		opt_val = 0;
+		bpf_map_update_elem(&option_count, &key, &opt_val, BPF_ANY);
+		break;
+	case BPF_TCP_PARSE_OPTIONS:
+		rcv_opt = bpf_ntohl(skops->args[2]);
+
+		/* Keep the first two bytes */
+		opt_val = rcv_opt >> 16;
+		key = 1;
+		bpf_map_update_elem(&option_count, &key, &opt_val, BPF_ANY);
+		break;
+	default:
+		rv = -1;
+	}
+	skops->reply = rv;
+	return 1;
+}
+char _license[] SEC("license") = "GPL";
