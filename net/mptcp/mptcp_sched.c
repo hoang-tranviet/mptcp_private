multipath-tcp/mptcp

commit 7a8e371e4f581914744a1ba407f9b9ce2d7b6528

mptcp: scheduler socket option
Previously, hosts have been limited to use the same scheduler for
all MPTCP connections. This patch enables a per-socket choice of
scheduler. Currently, this option cannot be used when a connection
has been established.

Signed-off-by: Per Hurtig <per.hurtig@kau.se>
Signed-off-by: Christoph Paasch <cpaasch@apple.com>

net/mptcp/mptcp_sched.c
@@ -554,18 +554,67 @@ int mptcp_set_default_scheduler(const char *name)
 	return ret;
 }
 
+/* Must be called with rcu lock held */
+static struct mptcp_sched_ops *__mptcp_sched_find_autoload(const char *name)
+{
+	struct mptcp_sched_ops *sched = mptcp_sched_find(name);
+#ifdef CONFIG_MODULES
+	if (!sched && capable(CAP_NET_ADMIN)) {
+		rcu_read_unlock();
+		request_module("mptcp_%s", name);
+		rcu_read_lock();
+		sched = mptcp_sched_find(name);
+	}
+#endif
+	return sched;
+}
+
 void mptcp_init_scheduler(struct mptcp_cb *mpcb)
 {
 	struct mptcp_sched_ops *sched;
+	struct sock *meta_sk = mpcb->meta_sk;
+	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
 
 	rcu_read_lock();
+	/* if scheduler was set using socket option */
+	if (meta_tp->mptcp_sched_setsockopt) {
+		sched = __mptcp_sched_find_autoload(meta_tp->mptcp_sched_name);
+		if (sched && try_module_get(sched->owner)) {
+			mpcb->sched_ops = sched;
+			goto out;
+		}
+	}
+
 	list_for_each_entry_rcu(sched, &mptcp_sched_list, list) {
 		if (try_module_get(sched->owner)) {
 			mpcb->sched_ops = sched;
 			break;
 		}
 	}
+out:
 	rcu_read_unlock();
+}
+
+/* Change scheduler for socket */
+int mptcp_set_scheduler(struct sock *sk, const char *name)
+{
+	struct mptcp_sched_ops *sched;
+	int err = 0;
+
+	rcu_read_lock();
+	sched = __mptcp_sched_find_autoload(name);
+
+	if (!sched) {
+		err = -ENOENT;
+	} else if (!ns_capable(sock_net(sk)->user_ns, CAP_NET_ADMIN)) {
+		err = -EPERM;
+	} else {
+		strcpy(tcp_sk(sk)->mptcp_sched_name, name);
+		tcp_sk(sk)->mptcp_sched_setsockopt = 1;
+	}
+	rcu_read_unlock();
+
+	return err;
 }
 
 /* Manage refcounts on socket close. */

include/linux/tcp.h
@@ -422,12 +422,15 @@ struct tcp_sock {
 		is_master_sk:1,
 		close_it:1,	/* Must close socket in mptcp_data_ready? */
 		closing:1,
-		mptcp_ver:4;
+		mptcp_ver:4,
+		mptcp_sched_setsockopt:1;
 	struct mptcp_tcp_sock *mptcp;
 #ifdef CONFIG_MPTCP
+#define MPTCP_SCHED_NAME_MAX 16
 	struct hlist_nulls_node tk_table;
 	u32		mptcp_loc_token;
 	u64		mptcp_loc_key;
+	char		mptcp_sched_name[MPTCP_SCHED_NAME_MAX];
 #endif /* CONFIG_MPTCP */
 };
 

include/net/mptcp.h
@@ -891,6 +891,7 @@ void mptcp_init_path_manager(struct mptcp_cb *mpcb);
 void mptcp_cleanup_path_manager(struct mptcp_cb *mpcb);
 void mptcp_fallback_default(struct mptcp_cb *mpcb);
 void mptcp_get_default_path_manager(char *name);
+int mptcp_set_scheduler(struct sock *sk, const char *name);
 int mptcp_set_default_path_manager(const char *name);
 extern struct mptcp_pm_ops mptcp_pm_default;
 

include/uapi/linux/tcp.h
@@ -116,6 +116,7 @@ enum {
 #define TCP_SAVE_SYN		27	/* Record SYN headers for new connections */
 #define TCP_SAVED_SYN		28	/* Get SYN headers recorded for connection */
 #define MPTCP_ENABLED		42
+#define MPTCP_SCHEDULER		43
 
 struct tcp_repair_opt {
 	__u32	opt_code;

net/ipv4/tcp.c
@@ -2434,6 +2434,34 @@ static int do_tcp_setsockopt(struct sock *sk, int level,
 		release_sock(sk);
 		return err;
 	}
+#ifdef CONFIG_MPTCP
+	case MPTCP_SCHEDULER: {
+		char name[MPTCP_SCHED_NAME_MAX];
+
+		if (optlen < 1)
+			return -EINVAL;
+
+		/* Cannot be used if MPTCP is not used or we already have
+		 * established an MPTCP-connection.
+		 */
+		if (mptcp_init_failed || !sysctl_mptcp_enabled ||
+		    sk->sk_state != TCP_CLOSE)
+			return -EPERM;
+
+		val = strncpy_from_user(name, optval,
+					min_t(long, MPTCP_SCHED_NAME_MAX - 1,
+					      optlen));
+
+		if (val < 0)
+			return -EFAULT;
+		name[val] = 0;
+
+		lock_sock(sk);
+		err = mptcp_set_scheduler(sk, name);
+		release_sock(sk);
+		return err;
+	}
+#endif
 	default:
 		/* fallthru */
 		break;
@@ -3005,6 +3033,25 @@ static int do_tcp_getsockopt(struct sock *sk, int level,
 		return 0;
 	}
 #ifdef CONFIG_MPTCP
+	case MPTCP_SCHEDULER:
+		if (get_user(len, optlen))
+			return -EFAULT;
+		len = min_t(unsigned int, len, MPTCP_SCHED_NAME_MAX);
+		if (put_user(len, optlen))
+			return -EFAULT;
+
+		if (mptcp(tcp_sk(sk))) {
+			struct mptcp_cb *mpcb = tcp_sk(mptcp_meta_sk(sk))->mpcb;
+
+			if (copy_to_user(optval, mpcb->sched_ops->name, len))
+				return -EFAULT;
+		} else {
+			if (copy_to_user(optval, tcp_sk(sk)->mptcp_sched_name,
+					 len))
+				return -EFAULT;
+		}
+		return 0;
+
 	case MPTCP_ENABLED:
 		val = sock_flag(sk, SOCK_MPTCP) ? 1 : 0;
 		break;

hoang-tranviet/mptcp_private

commit 5485e4e3a8de6610605b292c8570aae308270e19

mptcp: disable bh when setting scheduler
This was proposed by Christoph but was never fixed:
https://sympa-2.sipr.ucl.ac.be/sympa/arc/mptcp-dev/2016-11/msg00031.html

Fixes: 7a8e371e4f581 ("mptcp: scheduler socket option")

net/mptcp/mptcp_sched.c
@@ -552,9 +552,9 @@ static struct mptcp_sched_ops *__mptcp_sched_find_autoload(const char *name)
 	struct mptcp_sched_ops *sched = mptcp_sched_find(name);
 #ifdef CONFIG_MODULES
 	if (!sched && capable(CAP_NET_ADMIN)) {
-		rcu_read_unlock();
+		rcu_read_unlock_bh();
 		request_module("mptcp_%s", name);
-		rcu_read_lock();
+		rcu_read_lock_bh();
 		sched = mptcp_sched_find(name);
 	}
 #endif
@@ -593,7 +593,7 @@ int mptcp_set_scheduler(struct sock *sk, const char *name)
 	struct mptcp_sched_ops *sched;
 	int err = 0;
 
-	rcu_read_lock();
+	rcu_read_lock_bh();
 	sched = __mptcp_sched_find_autoload(name);
 
 	if (!sched) {
@@ -604,7 +604,7 @@ int mptcp_set_scheduler(struct sock *sk, const char *name)
 		strcpy(tcp_sk(sk)->mptcp_sched_name, name);
 		tcp_sk(sk)->mptcp_sched_setsockopt = 1;
 	}
-	rcu_read_unlock();
+	rcu_read_unlock_bh();
 
 	return err;
 }