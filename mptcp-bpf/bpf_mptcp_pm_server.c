85ab113f5c22196ce005a0edb4be891fd675de9d
Hoang Tran <hoang.tran@uclouvain.be>
2019-06-17 21:01:34 -0700

    pm_server prog: clean up

--- mptcp-bpf/bpf_mptcp_pm_server.c
+++ mptcp-bpf/bpf_mptcp_pm_server.c
@@ -21,11 +21,13 @@ SEC("sockops")
 int bpf_addaddr_server(struct bpf_sock_ops *skops)
 {
 	int rv = -1;
-	int op;
 	int v = 0;
-	char ret[] = "ret: %d\n";
 
-	op = (int) skops->op;
+	int op = (int) skops->op;
+	unsigned int token = skops->mptcp_loc_token;
+
+	char dbg[] = "server: call op: %d \n";
+	bpf_trace_printk(dbg, sizeof(dbg), op);
 
 	/* run on server only */
 	if (skops->local_port != 80)
@@ -33,41 +35,28 @@ int bpf_addaddr_server(struct bpf_sock_ops *skops)
 
 	switch (op) {
 	case BPF_MPTCP_FULLY_ESTABLISHED:
-	{
-		// this is not master sk, then skip it
+		/* skip master sk */
 		if (!skops->args[1])
 			break;
 
-		char fully[] = "%x: mptcp conn is fully established, is_master:%d\n";
-		bpf_trace_printk(fully, sizeof(fully),  skops->args[0],
-							skops->args[1]);
-		struct sockaddr_in *local_addr;
-
-		int key = 2;
-		local_addr = bpf_map_lookup_elem(&server_addr_map, &key);
-		if (!local_addr)
-			// without this check, verifier will reject
-			return 0;
-		char lookup[] = "local address: %u %x \n";
-		bpf_trace_printk(lookup, sizeof(lookup),
-				 bpf_ntohs(local_addr->sin_port),
-				 bpf_ntohl(local_addr->sin_addr.s_addr));
-		if (local_addr->sin_addr.s_addr == 0)
-			return 0;
-
+		char fully[] = "server: mpp conn is fully established, is_master:%d\n";
+		bpf_trace_printk(fully, sizeof(fully),  skops->args[1]);
 		break;
-	}
 	case BPF_MPTCP_ADDR_SIGNAL:
 	{
-		unsigned int size = skops->args[0];
+		unsigned int size = skops->args[1];
 		struct sockaddr_in *local_addr;
+
+		char deb[] = "server: ADDR_SIGNAL: size: %u \n";
+		bpf_trace_printk(deb, sizeof(deb), size);
 
 		int id = 2;
 		local_addr = bpf_map_lookup_elem(&server_addr_map, &id);
 		if (!local_addr)
 			return 0;
-		char fmt1[] = "%x: local address: %u, id: %u \n";
-		bpf_trace_printk(fmt1, sizeof(fmt1), bpf_ntohl(local_addr->sin_addr.s_addr), id);
+		char fmt1[] = "server: to send add_addr: %x, id: %d \n";
+		bpf_trace_printk(fmt1, sizeof(fmt1),
+				 bpf_ntohl(local_addr->sin_addr.s_addr), id);
 
 		rv = bpf_mptcp_addr_signal(skops, id,
 					(struct sockaddr *)local_addr,
